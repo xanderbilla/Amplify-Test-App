1f7d1735f2903a1119d3e6a400514194e2cf9383
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-18 23:12:20 +0530

    Test App

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,68 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "testApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
+    "testapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "testapp4ff1e71f4ff1e71f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminGroupRole": "string"
+    }
+  },
+  "function": {
+    "formfunc": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "testapp4ff1e71f4ff1e71fPostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "userdbfunc": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamo7dbf1653": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    },
+    "userTest": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { API, Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import './App.css'
 
-function App() {
+import awsExports from './aws-exports';
+import { useState } from 'react';
+Amplify.configure(awsExports);
+const App = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [DOB, setDOB] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const [address, setAddress] = useState('')
+  const [gender, setGender] = useState('')
+
+  async function addContact(event) {
+    event.preventDefault();
+    const myInit = {
+      body: {
+        name: name,
+        email: email,
+        phone: phone,
+        gender: gender,
+        dob: DOB,
+        avatar: avatar,
+        address: address
+      },
+    };
+
+    API.post('testApi', '/users', myInit)
+      .then((response) => {
+        console.log(response);
+        console.log("Sucessfully Added");
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+
+    /*
+    const data = {
+      name: name,
+      email: email,
+      phone: phone,
+      gender: gender,
+      dob: DOB,
+      avatar: avatar,
+      address: address
+    }
+    console.log(data );
+    */
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" name='Name' placeholder='Name' onChange={(e) => setName(e.target.value)} />
+        <input type="text" name='Email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+        <input type="number" name='Phone' placeholder='Phone' onChange={(e) => setPhone(e.target.value)} />
+        <select name="Gender" defaultValue={''} onChange={(e) => setGender(e.target.value)}>
+          <option value="">Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+        <input type="date" name="DOB" onChange={(e) => setDOB(e.target.value)} />
+        <input type="text" name='Avatar' placeholder='Avatar' onChange={(e) => setAvatar(e.target.value)} />
+        <input type="text" name='Address' placeholder='Address' onChange={(e) => setAddress(e.target.value)} />
+        <button type="button" onClick={addContact}>Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
